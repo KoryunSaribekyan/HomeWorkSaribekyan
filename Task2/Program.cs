7407ee84b8e39b0065e8ce7dcaa2f12d91d1da8e
Koryun <saribekyan.koryun@mail.ru>
2023-01-30 17:56:49 +0400

    Кроме доп задачи

---
+++ Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿//  Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int n1 = 0;
+int i = 10;
+while (i < 100000000)
+{
+    if (n >= i * 100 && n < i * 1000)
+    {
+        n1 = n / i;
+        Console.WriteLine(n1 % 10);
+    }
+    i = i * 10;
+}
+if (n >= 100 && n < 1000)
+{
+    Console.WriteLine(n % 10);
+}
+else if (n < 100)
+{
+    Console.WriteLine($"Третьей цифры не существует");
+}
---
+++ task3/Program.cs
@@ -0,0 +1,39 @@
+﻿/* Напишите программу, которая принимает на вход цифру, 
+обозначающую день недели, и проверяет, является ли этот день выходным.*/
+Console.Clear();
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n < 1 || n > 7)
+{
+    Console.Write("Вы ошиблись!\nВведите число: ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+if (n == 7)
+{
+    Console.Write($"Да.Воскресенье");
+}
+else if (n == 6)
+{
+    Console.Write($"Да.Суббота");
+}
+else if (n == 5)
+{
+    Console.Write($"Нет.Пятница");
+}
+else if (n == 4)
+{
+    Console.Write($"Нет.Четверг");
+}
+else if (n == 3)
+{
+    Console.Write($"Нет.Среда");
+}
+else if (n == 2)
+{
+    Console.Write($"Нет.Вторник");
+}
+else
+{
+    Console.Write($"Нет.Понедельник");
+}
